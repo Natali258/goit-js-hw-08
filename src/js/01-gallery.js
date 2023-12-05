53bae57dd446c216b5884d673eda618b72d7dec0
Natali258 <natalya.holyavko@gmail.com>
2023-12-05 23:34:18 +0200

    hw-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const list = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" /></a></li>`;
+  })
+  .join('');
+
+const galleryUl = document.querySelector('.gallery');
+galleryUl.insertAdjacentHTML('beforeend', list);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
 
+const getFormValue = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+const form = document.querySelector('.feedback-form');
+let formValue = {};
+
+if (getFormValue) {
+  form.elements.email.value = getFormValue.email || '';
+  form.elements.message.value = getFormValue.message || '';
+  formValue = getFormValue;
+}
+
+const hangleChange = evt => {
+  const { name, value } = evt.target;
+  (formValue[name] = value),
+    localStorage.setItem('feedback-form-state', JSON.stringify(formValue));
+};
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(formValue);
+  localStorage.removeItem('feedback-form-state');
+  evt.currentTarget.reset();
+  formValue = {};
+}
+
+form.addEventListener('input', throttle(hangleChange, 500));
+form.addEventListener('submit', onFormSubmit);
